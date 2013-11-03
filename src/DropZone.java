136b1dba1e33d1b148d9b68f58642c62d4a5f592
Josh Eversmann <josh.eversmann@utexas.edu>
2013-11-03 23:04:39 +0000

    Building graph

--- src/DropZone.java
+++ src/DropZone.java
@@ -7,7 +7,7 @@ public class DropZone {
         Scanner in = new Scanner(System.in);
         int caseCount = in.nextInt();
         for(int testCount = 0; testCount < caseCount; testCount++) {
-            int h = in.nextInt(), w = in.nextInt();
+            int h = in.nextInt(), w = in.nextInt(), n = 1;
             int[][] field = new int[h][w];
             Pattern delim = in.delimiter();
             in.useDelimiter("");
@@ -18,7 +18,7 @@ public class DropZone {
                             field[y][x] = -1;
                             break;
                         case '.':
-                            field[y][x] = 1;
+                            field[y][x] = n++;
                             break;
                         case 'D':
                             field[y][x] = 0;
@@ -29,9 +29,26 @@ public class DropZone {
                 }
             }
             in.useDelimiter(delim);
-            System.out.printf("Test case: %d\n", testCount);
-            for(int[] row: field) {
-                System.out.println(Arrays.toString(row));
+            //System.out.printf("Test case: %d\n", testCount);
+            //for(int[] row: field) {
+            //    System.out.println(Arrays.toString(row));
+            //}
+            MaxFlowGraph zone = new MaxFlowGraph();
+            for(int y = 0; y < h; y++) {
+                for(int x = 0; x < w; x++) {
+                    int node = field[y][x];
+                    if(node != -1) {
+                        int[] nexts = new int[4];
+                        nexts[0] = y == 0 ? -1 : field[y-1][x];
+                        nexts[0] = x == 0 ? -1 : field[y][x-1];
+                        nexts[0] = y == h - 1 ? -1 : field[y+1][x];
+                        nexts[0] = x == w - 1 ? -1 : field[y][x+1];
+                        for(int sides = 0; sides < 4; sides++) {
+                            //Over adds edges without checking the return condition
+                            zone.addEdge(node, nexts[sides], 1);
+                        }
+                    }
+                }
             }
         }
     }
--- src/MaxFlowGraph.java
+++ src/MaxFlowGraph.java
@@ -1,4 +1,55 @@
+import java.util.*;
 
 public class MaxFlowGraph {
 
+    private HashMap<Integer,Node> adjacency;
+
+    public MaxFlowGraph() {
+        adjacency = new HashMap<Integer,Node>();
+    }
+
+    public boolean addEdge(int left, int right, int weight) {
+        Node lNode;
+        Node rNode;
+        if(adjacency.containsKey(left)) {
+            lNode = adjacency.get(left);
+        } else {
+            lNode = new Node(left);
+        }
+        if(adjacency.containsKey(right)) {
+            rNode = adjacency.get(right);
+        } else {
+            rNode = new Node(right);
+        }
+        return rNode.addEdge(left, weight) & lNode.addEdge(right, weight);
+    }
+
+    public int getFlow(int left, int right) {
+        if(!adjacency.containsKey(left) || !adjacency.containsKey(right)) {
+            return -1;
+        }
+
+        return 0;
+    }
+
+    private class Node {
+
+        private HashMap<Integer,Integer> myEdges;
+        private int index;
+
+        public Node(int i) {
+            index = i;
+            myEdges = new HashMap<Integer, Integer>();
+        }
+
+        public boolean addEdge(int dest, int weight) {
+            if(myEdges.containsKey(dest)) {
+                return false;
+            } else {
+                myEdges.put(dest, weight);
+                return true;
+            }
+        }
+
+    }
 }
